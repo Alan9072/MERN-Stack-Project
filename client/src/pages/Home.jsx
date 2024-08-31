ebd76b1a5f0605fc61a525d44d1ca5b5185d60b6
Alan Joy <alanjoy456@gmail.com>
2024-08-31 21:20:17 +0530

    taken values

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
 import styles from "./Home.module.css";
+import { MdEdit } from "react-icons/md";
+import { MdDelete } from "react-icons/md";
 
 function Home() {
+  const [input,setInput] = useState('');
+  const [arr,setArr] = useState([]);
+  const handleChange = (e) => {
+    setInput(e.target.value);
+    console.log(e.target.value);
+  };
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+      setArr([...arr,input]);
+      setInput('');
+  }
+
   return (
     <div className={styles.HomeContainer}>
       <div className={styles.Homebox}>
         <Navbar />
         <div className={styles.insider}>
-          <form action="">
-            <input type="text" />
-            <button>Add</button>
+          <form action="" onSubmit={handleSubmit}>
+            <input type="text" value={input} onChange={handleChange} placeholder="Enter task"/>
+            <button type="submit">Add</button>
           </form>
           <div className={styles.contentinside}>
-            
+            {arr.length === 0? 
+            <div className={styles.divdes}>Nothing</div>:
+            arr.map((item, index) => (
+              <div className={styles.divdes} key={index}>
+                <div className={styles.textdiv}>
+                  {item}
+                </div>
+                <div className={styles.button}>
+                  <MdEdit/>
+                  <MdDelete/>
+                </div>
+              </div> // Display each item in the array
+            ))
+            }
+          
           </div>
         </div>
       </div>
