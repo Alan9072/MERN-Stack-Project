6f6ed36e3a21b2f24ab3a966ac49c6f2d425ac22
Alan Joy <alanjoy456@gmail.com>
2024-09-01 15:17:04 +0530

    just tried link

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import { IoIosMenu } from "react-icons/io";
+import { IoIosMenu  } from "react-icons/io";
+import {Link} from 'react-router-dom'
 import styles from './Navbar.module.css'
 
 function Navbar() {
   return (
     <div className={styles.NavArea}>
         <div className={styles.todologo}><h3>ToDo</h3></div>
-        <div className={styles.menu}><IoIosMenu/></div>
+        <Link  className={styles.link} to="/login">
+        <div className={styles.menu}><IoIosMenu/><p>Login</p></div>
+        </Link>
+        
     </div>
   )
 }
