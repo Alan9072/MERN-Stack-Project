50aea0c2e833fce4fde8cb069deda06a2ef129f1
Alan Joy <alanjoy456@gmail.com>
2024-09-02 21:25:54 +0530

    set the origin for cors policy

--- api/index.js
+++ api/index.js
@@ -1,26 +1,33 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import Task from './Models/task.js'
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import Task from "./Models/task.js";
 
 dotenv.config();
 const port = 5000;
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://react-project-frontend-mu.vercel.app"], // Replace with your domains
+    methods: "GET,POST,PUT,DELETE", // Allowed methods
+    credentials: true, // Allow cookies to be sent with requests
+     // Some legacy browsers choke on 204
+  })
+);
+
 app.use(express.urlencoded({ extended: true }));
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URL)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
-
-
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 // Routes
-app.post('/tasks', async (req, res) => {
+app.post("/tasks", async (req, res) => {
   try {
     const { task } = req.body;
     const newTask = new Task({ task });
@@ -31,7 +38,7 @@ app.post('/tasks', async (req, res) => {
   }
 });
 
-app.get('/tasks', async (req, res) => {
+app.get("/tasks", async (req, res) => {
   try {
     const tasks = await Task.find();
     res.json(tasks);
@@ -40,18 +47,22 @@ app.get('/tasks', async (req, res) => {
   }
 });
 
-app.put('/tasks/:id', async (req, res) => {
+app.put("/tasks/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const { task } = req.body;
-    const updatedTask = await Task.findByIdAndUpdate(id, { task }, { new: true });
+    const updatedTask = await Task.findByIdAndUpdate(
+      id,
+      { task },
+      { new: true }
+    );
     res.json(updatedTask);
   } catch (error) {
     res.status(500).send(error.message);
   }
 });
 
-app.delete('/tasks/:id', async (req, res) => {
+app.delete("/tasks/:id", async (req, res) => {
   try {
     const { id } = req.params;
     await Task.findByIdAndDelete(id);
