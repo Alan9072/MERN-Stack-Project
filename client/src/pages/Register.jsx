cfe300087fa509e9df794a48822cec478acad2b3
Alan Joy <alanjoy456@gmail.com>
2024-09-02 22:46:32 +0530

    chane

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+
+function Register() {
+  return (
+    <div><Header value={"Register"}/></div>
+  )
+}
+
+export default Register
